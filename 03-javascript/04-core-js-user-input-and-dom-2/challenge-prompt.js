3e16e0c82d465e9ee9f21693faf108c17682c7ac
vaguilar25 <vivian.aguilar@gmail.com>
2018-11-06 17:14:29 -0600

    activity 4

--- 03-javascript/04-core-js-user-input-and-dom-2/challenge-prompt.js
+++ 03-javascript/04-core-js-user-input-and-dom-2/challenge-prompt.js
@@ -5,23 +5,25 @@
 // Using the `confirm` method, welcome the user to the Boba Shop and ask them if they would like a boba
 // Store the user response in a variable `wantsBoba`
 
+var wantsBoba = confirm ("Welcome to the Boba Shop, Do you like a boba?");
 
 // Create an if statement checking whether the user wanted boba from the earlier `confirm` dialogue
-
-
+if (wantsBoba === true) {
 
     // Declare a variable `drinkFlavor`
-
+    var drinkFlavor;
     
     // Prompt the user for what flavor they would like for their drink
     // Store their response into the variable `drinkFlavor`
 
-    
+    drinkFlavor=prompt("What would you like for drink?");
     
     // Write to the DOM a message to the user telling them that their drink will be ready soon.
     // Use string concatenation in order to specify the flavor of their drink
-
+    document.write("Your " + drinkFlavor + " will be ready soon");
     
 // Else, write "Goodbye! Come back soon!" to the DOM
+} else {
+    document.write ("Goodbye");
 
-
+}
