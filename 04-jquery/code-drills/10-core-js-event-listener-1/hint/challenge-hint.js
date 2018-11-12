a06a90bf7b02ace6c30f5d6243bfa4a42f7c94ec
vaguilar25 <vivian.aguilar@gmail.com>
2018-11-11 19:44:48 -0600

    code drill 10

--- 04-jquery/code-drills/10-core-js-event-listener-1/hint/challenge-hint.js
+++ 04-jquery/code-drills/10-core-js-event-listener-1/hint/challenge-hint.js
@@ -1,24 +1,41 @@
 // declare the variable we're storing the confirm dialogue in
-
+var dialogueAnswer;
 
 // create a key press listener
-
+document.onkeyup = function (event) {
 
   // store the key the user pressed into a variable so we can 
   // refer to it easier in this function and
   // change it to lower case to make our logic not case sensitive
-
+var userKeyPress = event.key;
 
   // console log the key that the user pressed
-
+console.log("Key press" + userKeyPress);
 
   // check whether the user pressed the "h" key
+if (userKeyPress === "h") {
+   // create a confirm dialogue when they do and store the response from that
+  // dialogue into the variable we declared outside the event listener
+   // $("body").append("<div>Confirm</div>");
+    dialogueAnswer = confirm("Confirm");
+//  function createDialog(title, text, options) {
+  //  return $("<div class='dialog' title='" + title + "'><p>" + text + "</p></div>")
+    //.dialog(options);
+//}
+//createDialog("test","testing", "options");
 
-    // create a confirm dialogue when they do and store the response from that
-    // dialogue into the variable we declared outside the event listener
 
-
-
-  // check whether the user pressed the "k" key
+} 
+// check whether the user pressed the "k" key
     // check whether the user had answered true to the previous confirm dialogue
     // display the corresponding alert dialogue
+if (userKeyPress === "k") {
+   if (dialogueAnswer === true) {
+     alert("Ok");
+   } else {
+     alert("No");
+   }
+  
+}
+
+}
