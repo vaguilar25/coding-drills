7488fa9733664024926009d00d1dbed8bda93ab9
vaguilar25 <vivian.aguilar@gmail.com>
2018-11-14 18:06:02 -0600

    code drill 01

--- 05-timers/code-drills/01-flash-jquery-input/challenge.js
+++ 05-timers/code-drills/01-flash-jquery-input/challenge.js
@@ -1,7 +1,27 @@
 $(function () {
 
-    $( "#contactData" ).submit(function( ) {
-        $("#name").text("#firstname");
+    $(document).on("click", "button", function (event) {
+        event.preventDefault();
+
+        var firstName = $("#firstName").val();
+        var lastName = $("#lastName").val();
+        var occupation = $("#occupation").val();
+        var city=$("#city").val();
+        var state=$("#state").val();
+
+        $("#firstName").val("");
+        $("#lastName").val("");
+
+        $("#occupation").val("");
+        $("#city").val("");
+        $("#state").val("");
+
+        var nameDiv = $("<h3>").text(firstName + " " + lastName);
+        var occupationDiv = $("<h3>").text(occupation );
+        var cityDiv= $("<h3>").text(city);
+        var stateDiv= $("<h3>").text(state);
+
+        $("#display-area").append(nameDiv,occupationDiv,cityDiv, stateDiv);
     });
 
 })
