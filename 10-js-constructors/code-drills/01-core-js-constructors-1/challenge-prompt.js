27014e9257c3a40ddab1db1e7d99367101b09577
vaguilar25 <vivian.aguilar@gmail.com>
2018-12-11 11:55:58 -0600

    code drills

--- 10-js-constructors/code-drills/01-core-js-constructors-1/challenge-prompt.js
+++ 10-js-constructors/code-drills/01-core-js-constructors-1/challenge-prompt.js
@@ -1,20 +1,42 @@
+var shuffle = require('fisher-yates')
 // Defining the card constructor function
 //
 // -------------------- Your Code Here --------------------
+var Card = function (value) {
 
+    if (value >= 1 && value <= 13) {
+        this.value = value;
+    } else {
+        this.value = 1;
+    }
 
-
-
+    this.print = function () {
+        console.log(this.value);
+    };
+};
 
 
 // --------------------- End Code Area --------------------
-
-
-
 // Defining the deck constructor function
 //
 // -------------------- Your Code Here --------------------
+var Deck = function ( cards = []) {
+    this.cards = cards;
+    this.count = function () {
+        return this.cards.length;
+    }
+    this.shuffle = function () {
+        shuffle(this.cards);
+    }
+    this.draw = function () {
+        return this.cards.pop();
+    }
+    this.add = function (value) {
 
+        this.cards.push(new Card(value));
+        this.shuffle();
+    }
+}
 
 
 
@@ -30,5 +52,6 @@
 
 
 
-
+module.exports.Card = Card;
+module.exports.Deck = Deck;
 // --------------------- End Code Area --------------------
